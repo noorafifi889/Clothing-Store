73ad36b10420e6ff350ade104c4f711ad39d0f31
noorafifi889 <noorelafifi889@gmail.com>
2025-10-22 15:43:31 +0300

    change a word

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const container = document.getElementById("products-container");
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => {
+    let product = data;
+    console.log(product);
     data.forEach((product) => {
       const card = document.createElement("div");
       card.className = "col";
       let title = product.title;
-      if (title.length > 50) {
-        title = title.substring(0, 40) + "...";
+      if (title.length > 30) {
+        title = title.substring(0, 30) + "...";
       }
       card.innerHTML = `
         <a href="product.html?id=${product.id}" class="product" target="_blank" style="text-decoration: none; color: black;">
@@ -19,7 +21,7 @@ fetch("https://fakestoreapi.com/products")
               <p class="card-text">${product.price} $</p>
               <p class="text-black-50">Product Code: ${product.id}</p>
               <div class="icon fs-4 d-flex justify-content-between align-items-center mr-5" style="margin-right: 16%">
-                <a href="#" class="icon-style"><i class="bi bi-cart-check icon-style"></i></a>
+                <a href="#" class="icon-style add-cart" data-id="${product.id}"><i class="bi bi-cart-check icon-style"></i></a>
                 <a href="#" class="icon-style"><i class="bi bi-heart icon-style"></i></a>
                 <a href="#" class="icon-style"><i class="bi bi-share icon-style"></i></a>
               </div>
@@ -29,5 +31,39 @@ fetch("https://fakestoreapi.com/products")
       `;
       container.appendChild(card);
     });
+    // let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const cart_icon = document.getElementById("cart-icon");
+    let count = 0;
+    console.log(cart_icon);
+    // الآن أضف الأحداث لجميع الأزرار
+    const addCartButtons = document.querySelectorAll(".add-cart");
+
+    addCartButtons.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        count++;
+        cart_icon.innerHTML = `<i class="bi bi-cart"></i>` + " (" + count + ")";
+      });
+      // localStorage.setItem("cart", JSON.stringify(cartArray));
+    });
   })
   .catch((err) => console.error("حدث خطأ أثناء جلب البيانات:", err));
+function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+function saveCart(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+function addItemToCart(product) {
+  let cart = [];
+  let rows = document.getElementsByClassName("row-1");
+  let cart_info = document.createElementNS("cart-info");
+  cart_info.innerHTML = ` <img id="cart-img" src="img/190781.jpg" alt="" width="100px" height="100px">
+             <div id="cart-title" class="title fw-bold">Labptop</div>
+             <div id="cart-price" class="price fw-bold">100$ </div>
+            <button id="delete-btn" class="btn btn-danger fw-bold">delete</button>
+             `;
+  rows.appendChild(cart_info);
+}
